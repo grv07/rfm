25ad56dc8b042b7c3591f7662a5a054d5a4e8d9a
grv07 <grvtyagi22@gmail.com>
2021-11-27 12:31:08 +0530

    change layout and add style

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 use std::io;
 use termion::raw::IntoRawMode;
-use tui::Terminal;
 use tui::backend::TermionBackend;
-use tui::widgets::{Widget, Block, Borders};
-use tui::layout::{Layout, Constraint, Direction};
+use tui::layout::{Constraint, Direction, Layout};
+use tui::style::{Color, Modifier, Style};
+use tui::text::Span;
+use tui::widgets::{Block, Borders};
+use tui::Terminal;
+
+fn title_style() -> Style {
+    Style::default()
+        .fg(Color::LightGreen)
+        .bg(Color::Black)
+        .add_modifier(Modifier::BOLD)
+}
+
+fn get_title_span(text: &str) -> Span {
+    Span::styled(text, title_style())
+}
 
 fn main() -> Result<(), io::Error> {
     let stdout = io::stdout().into_raw_mode()?;
@@ -11,23 +24,24 @@ fn main() -> Result<(), io::Error> {
     let mut terminal = Terminal::new(backend)?;
     terminal.draw(|f| {
         let chunks = Layout::default()
-            .direction(Direction::Vertical)
+            .direction(Direction::Horizontal)
             .margin(1)
             .constraints(
                 [
                     Constraint::Percentage(10),
-                    Constraint::Percentage(80),
-                    Constraint::Percentage(10)
-                ].as_ref()
+                    Constraint::Percentage(90),
+                    Constraint::Percentage(10),
+                ]
+                .as_ref(),
             )
             .split(f.size());
         let block = Block::default()
-             .title("Block")
-             .borders(Borders::ALL);
+            .title(get_title_span("Dir"))
+            .borders(Borders::ALL);
         f.render_widget(block, chunks[0]);
         let block = Block::default()
-             .title("Block 2")
-             .borders(Borders::ALL);
+            .title(get_title_span("Files"))
+            .borders(Borders::ALL);
         f.render_widget(block, chunks[1]);
     })?;
     Ok(())
