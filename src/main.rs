ba9c293280d30d50484d45862f90e1c6e148a246
grv07 <grvtyagi22@gmail.com>
2021-11-28 11:18:32 +0530

    get file list with depth 1 and display in layout

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,14 @@ use tui::backend::TermionBackend;
 use tui::layout::{Constraint, Direction, Layout};
 use tui::style::{Color, Modifier, Style};
 use tui::text::Span;
-use tui::widgets::{Block, Borders};
+use tui::widgets::{Block, Borders, List, ListItem};
 use tui::Terminal;
+
+use walkdir::WalkDir;
+
+//TASKS:
+// Keep all of the style in seprate mod.
+// Create a mod for layout building only.
 
 fn title_style() -> Style {
     Style::default()
@@ -18,6 +24,26 @@ fn get_title_span(text: &str) -> Span {
     Span::styled(text, title_style())
 }
 
+fn get_files_list(path: &str) -> Vec<ListItem> {
+    let mut list_item = Vec::new();
+    for entry in WalkDir::new(path)
+        .max_depth(1)
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        let curr_file = entry.path().to_string_lossy().into_owned();
+        list_item.push(ListItem::new(curr_file));
+    }
+    list_item
+}
+
+fn tree_widget(block: Block) -> List {
+    List::new(get_files_list("/home/tyagig/rfm"))
+        .block(block)
+        .highlight_symbol(">>")
+        .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
+}
+
 fn main() -> Result<(), io::Error> {
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
@@ -26,10 +52,12 @@ fn main() -> Result<(), io::Error> {
         let chunks = Layout::default()
             .direction(Direction::Horizontal)
             .margin(1)
+            .horizontal_margin(5)
+            .vertical_margin(5)
             .constraints(
                 [
-                    Constraint::Percentage(10),
-                    Constraint::Percentage(90),
+                    Constraint::Percentage(40),
+                    Constraint::Percentage(50),
                     Constraint::Percentage(10),
                 ]
                 .as_ref(),
@@ -38,7 +66,7 @@ fn main() -> Result<(), io::Error> {
         let block = Block::default()
             .title(get_title_span("Dir"))
             .borders(Borders::ALL);
-        f.render_widget(block, chunks[0]);
+        f.render_widget(tree_widget(block), chunks[0]);
         let block = Block::default()
             .title(get_title_span("Files"))
             .borders(Borders::ALL);
