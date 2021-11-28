122eea0edc41f96a4ba2a232f80665fdbda1bcf7
grv07 <grvtyagi22@gmail.com>
2021-11-28 22:49:03 +0530

    change backend to crossterm and change basic template to infinite loop to render and quit app with q

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use std::io;
-use termion::raw::IntoRawMode;
-use tui::backend::TermionBackend;
-use tui::layout::{Constraint, Direction, Layout};
-use tui::style::{Color, Modifier, Style};
-use tui::text::Span;
-use tui::widgets::{Block, Borders, List, ListState, ListItem};
-use tui::Terminal;
-
+use crossterm::{
+    event::{read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{error::Error, io};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::Span,
+    widgets::{Block, Borders, List, ListItem, ListState},
+    Frame, Terminal,
+};
 use walkdir::WalkDir;
 
 //TASKS:
@@ -51,36 +56,67 @@ fn tree_widget(block: Block) -> List {
         .highlight_style(selected_dir_style())
 }
 
-fn main() -> Result<(), io::Error> {
-    let stdout = io::stdout().into_raw_mode()?;
-    let backend = TermionBackend::new(stdout);
+fn main() -> Result<(), Box<dyn Error>> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-    terminal.draw(|f| {
-        let chunks = Layout::default()
-            .direction(Direction::Horizontal)
-            .margin(1)
-            .horizontal_margin(5)
-            .vertical_margin(5)
-            .constraints(
-                [
-                    Constraint::Percentage(40),
-                    Constraint::Percentage(50),
-                    Constraint::Percentage(10),
-                ]
-                .as_ref(),
-            )
-            .split(f.size());
-        let block = Block::default()
-            .title(get_title_span("Dir"))
-            .borders(Borders::ALL);
-        let mut state = ListState::default();
-        state.select(Some(2));
-        f.render_stateful_widget(tree_widget(block), chunks[0], &mut state);
-        let block = Block::default()
-            .title(get_title_span("Files"))
-            .borders(Borders::ALL);
-        f.render_widget(block, chunks[1]);
-    })?;
+
+    // run app
+    let res = run_app(&mut terminal);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+    if let Err(err) = res {
+        println!("{:?}", err);
+    }
     Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| ui(f))?;
+        if let Event::Key(key_event) = read()? {
+            match key_event.code {
+                KeyCode::Char('q') => return Ok(()),
+                _ => {}
+            }
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>) {
+    let chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .margin(1)
+        .horizontal_margin(5)
+        .vertical_margin(5)
+        .constraints(
+            [
+                Constraint::Percentage(40),
+                Constraint::Percentage(50),
+                Constraint::Percentage(10),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
+    let block = Block::default()
+        .title(get_title_span("Dir"))
+        .borders(Borders::ALL);
+    let mut state = ListState::default();
+    state.select(Some(2));
+    f.render_stateful_widget(tree_widget(block), chunks[0], &mut state);
+    let block = Block::default()
+        .title(get_title_span("Files"))
+        .borders(Borders::ALL);
+    f.render_widget(block, chunks[1]);
 }
 
